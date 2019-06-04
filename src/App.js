86d710d757a8c3116d4205fae150f7205832b6de
Shareef <shareefnuseibeh@yahoo.com>
2019-06-04 04:35:54 +0300

    slider in progress

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import { withStyles, Theme } from '@material-ui/core/styles';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    top: '50%',
+    right: -3,
+    // The border color match the background color.-
+    border: `2px solid ${
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[900]
+    }`,
+  },
+}))(Badge);
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+`;
+
+const Page = styled.div`
+  width: 10%;
+  padding: 10px;
+`;
 
 class App extends Component{
   constructor(props){
@@ -36,14 +59,15 @@ class App extends Component{
       <AppBar position="static">
         <Toolbar>
           <Typography>
-          <img src={logo} style={{width:"150px",height:"120px", paddingRight:"25%"}} alt="logo" />
+          <img src={logo} style={{width:"150px",height:"120px", paddingRight:"25%"}} href = "#" alt="logo" />
           </Typography>
           <Typography className={this.classes.title} style={{marginLeft: "80%", position: 'absolute'}}>
           <Button color="inherit">Login</Button>
           <Button color="inherit">Register</Button>
         <IconButton aria-label="Cart" color="inherit">
-          <ShoppingCartIcon />
-        <h6>2 JD</h6>
+        <StyledBadge badgeContent={4} color="primary">
+        <ShoppingCartIcon />
+      </StyledBadge>
         </IconButton>
           </Typography>
         </Toolbar>
@@ -56,10 +80,18 @@ class App extends Component{
       >      
         <Tab label="Groceries" />
         <Tab label="Elctronics" />
-        <Tab label="Beauty and Personal Care" />
+        <Tab label="Beauty & Personal Care" />
         <Tab label="Excersie / Workout"/>
       </Tabs>
     </Paper>
+    <h2>Special Offers</h2>
+    <Wrapper>
+      <Slider speed={300} slidesToShow={1} slidesToScroll={1} infinite={false} draggable={true} centerMode={true}>
+        <Page>one</Page>
+        <Page>two</Page>
+        <Page>three</Page>
+      </Slider>
+    </Wrapper>
     </div>
     )
   }
