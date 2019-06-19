a397570511c7e6dc25be6d51fa2ae4981f7c3fa6
Shareef <macbookair@Macbooks-Air.Dlink>
2019-06-19 19:32:04 +0300

    map updates

--- src/components/map.js
+++ src/components/map.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
  
 export class MapComponent extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
 			text: '',
+			modalIsOpen: false,
 		}
+		this.handleClickOpen = this.handleClickOpen.bind(this);
 	}
 
 	handelInputChange(event){
 		this.setState({
 			[event.target.name]: event.target.value
+		})
+	}
+
+	handleClickOpen(){
+		this.setState({
+			modalIsOpen: ! this.state.modalIsOpen
 		})
 	}
 
@@ -34,7 +52,28 @@ export class MapComponent extends Component {
  				name="text"
 				autoFocus
  			/>
-			 <Button 	variant="contained" color="primary" style={{marginLeft: '35%', marginTop: '20px'}}>Request Order</Button>
+			 <Button variant="contained" color="primary" style={{marginLeft: '35%', marginTop: '20px'}} onClick={this.handleClickOpen}>Request Order</Button>
+			</div>
+			<div>
+			<Dialog
+        open={this.state.modalIsOpen}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title" style={{textAlign: 'center'}}></DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description" style={{textAlign: 'center', color: 'black'}}><strong>
+            Thank you for choosing to shop with GoMarket, Your order has been sent and our team will be there shortly</strong>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button href="/" color="primary">
+            Home
+          </Button>
+        </DialogActions>
+      </Dialog>
 			</div>
 			</div>
     );
