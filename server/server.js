63dd4da756ee11f5ff8309c7e967400b2ebc422e
Shareef <macbookair@Macbooks-Air.Dlink>
2019-06-26 13:42:54 +0300

    deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const users = require('./router');
 const port = process.env.PORT || 6060;
 const app = express().use('*', cors());
 
-app.use(express.static(__dirname + '../build/static'));
+app.use(express.static(__dirname + '../build/static/js'));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
