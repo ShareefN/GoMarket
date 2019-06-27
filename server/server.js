73bac613ea01ba3f57e95866fc309ba3d5d9f37d
Shareef <macbookair@Macbooks-Air.Dlink>
2019-06-27 17:09:43 +0300

    deployment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
+const Nexmo = require('nexmo')
 const users = require('./router');
 const port = process.env.PORT || 6060;
 const app = express().use('*', cors());
-const path = require("path")
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
@@ -14,21 +14,12 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	 next();
   });
-
-app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, "../build")));
-app.get('/', function (req, res) {
- return res.send('/home');
-});
+  app.use(express.static("../build"))
 
 app.use('/', users.router)
-app.get('/', (req, res) => {
-	res.send('/')
+app.get("/*", (req, res) => {
+	res.send("index.html")
 })
-app.get('/*', function (req, res) {
-	//res.sendFile(path.join(__dirname + 'build', 'index.html'));
-	res.sendFile('./public/index.html')
-  });
 
 app.listen(port, () => {
 	console.log(`Conneceted to port ${port}`);
