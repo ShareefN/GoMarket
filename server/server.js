f462a7e03eff840bc4ba7d8f964233a36862d252
Shareef <macbookair@macbooks-macbook-air.home>
2019-06-28 16:29:21 +0300

    localhost 300 working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+// const { createServer } = require('http');
+// const compression = require('compression');
+// const morgan = require('morgan');
+// const path = require('path');
 const bodyparser = require('body-parser');
-const Nexmo = require('nexmo')
 const users = require('./router');
+
 const port = process.env.PORT || 6060;
+
 const app = express().use('*', cors());
+// const dev = app.get('env') !== 'production'
+
+// if(!dev){
+// 	app.disable('x-powered-by')
+// 	app.use(compression())
+// 	app.use(morgan('common'))
+
+// 	app.use(express.static(path.resolve(__dirname, 'build')))
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+// 	})
+// }
+
+// if(dev){
+// 	app.use(morgan('dev'))
+// }
+
+// const server = createServer(app)
+
+// server.listen(port , err => {
+// 	if(err) throw err;
+// })
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
